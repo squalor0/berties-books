e5fe053e04260f26dfc01b36069487ddc61470b2
Blake Murphy <bmurp001@gold.ac.uk>
2025-11-19 14:03:58 +0000

    updated as per email

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // Import express and ejs
+require('dotenv').config();
+
 var express = require ('express')
 var ejs = require('ejs')
 const path = require('path')
@@ -23,9 +25,9 @@ app.locals.shopData = {shopName: "Bertie's Books"}
 // Define the database connection pool
 const db = mysql.createPool({
     host: 'localhost',
-    user: 'berties_books_app',
-    password: 'qwertyuiop',
-    database: 'berties_books',
+    user: process.env.BB_USER,
+    password: process.env.BB_PASSWORD,
+    database: process.env.BB_DATABASE,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
